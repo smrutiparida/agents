40d6302ea6eabf2565726f10afb8ee907e62c6ed
smruti <diaspora@ip-172-31-23-159.ap-northeast-1.compute.internal>
2014-03-25 09:28:39 +0000

    script to listen to XMPP requests

---
+++ Gemfile
@@ -0,0 +1,14 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.13'
+
+# Bundle edge Rails instead:
+# gem 'rails', :git => 'git://github.com/rails/rails.git'
+
+
+
+
+gem 'daemons'
+
+
+
---
+++ railways/pnr.rb
@@ -0,0 +1,28 @@
+# myapp.rb
+require "active_support/core_ext"
+require 'rubygems'
+require 'agent_xmpp'
+require 'net/http'
+require 'json'
+
+chat do
+  out = Net::HTTP.get('lmnopapp.com','/railways/pnr?q=' + params[:body])
+#  print out
+  res = []
+  res = JSON.parse(out) unless out.empty?
+  out_status = ""
+  if res.empty?
+    out_status = "Sorry, problem in getting status"
+  else
+    res.each do |x| 
+      out_status += x["status"] if x.has_key?("status") and !x["status"].nil?
+      out_status += ", Berth: " + x["berth"] + "\n" if x.has_key?("berth") and !x["berth"].nil?
+    end
+  end
+  out_status
+#  res.body
+#  params[:body].reverse
+end
+#command 'helo' do
+# 'Hello World' 
+#end
